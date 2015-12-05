59e65daa9d443a0e158a30886f32339d7a89b72f
张文健 <zwj777@live.com>
2015-12-05 15:29:53 +0800

    微信支付

---
+++ WxPayAPI/WxPayAPI/lib/Config.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace WxPayAPI
+{
+    /**
+    * 	配置账号信息
+    */
+    public class WxPayConfig
+    {
+        //=======【基本信息设置】=====================================
+        /* 微信公众号信息配置
+        * APPID：绑定支付的APPID（必须配置）
+        * MCHID：商户号（必须配置）
+        * KEY：商户支付密钥，参考开户邮件设置（必须配置）
+        * APPSECRET：公众帐号secert（仅JSAPI支付的时候需要配置）
+        */
+        public const string APPID = "wxcc249bdf42e4d7e3";//"wxcc249bdf42e4d7e3";
+        public const string MCHID = "1231136702";// "1231136702";
+        public const string KEY = "2f5c82fcr9a6ae11e5ca512d005056ae";//"E6CF121DF5F5F8AEF8DB5AF28473E6CF";
+        public const string APPSECRET = "a2f1e22a0dd54e5050181b037a7cfc06";// "a2f1e22a0dd54e5050181b037a7cfc06";
+
+        //=======【证书路径设置】===================================== 
+        /* 证书路径,注意应该填写绝对路径（仅退款、撤销订单时需要）
+        */
+        public const string SSLCERT_PATH = "cert/apiclient_cert.p12";
+        public const string SSLCERT_PASSWORD = "1233410002";
+
+
+
+        //=======【支付结果通知url】===================================== 
+        /* 支付结果通知回调url，用于商户接收支付结果wxcc249bdf42e4d7e3
+        */
+        public const string NOTIFY_URL = "http://hshop.wochu.cn/wxpaytest/example/resultnotifypage.aspx";
+
+        //=======【商户系统后台机器IP】===================================== 
+        /* 此参数可手动配置也可在程序中自动获取
+        */
+        public const string IP = "58.247.11.229";
+
+
+        //=======【代理服务器设置】===================================
+        /* 默认IP和端口号分别为0.0.0.0和0，此时不开启代理（如有需要才设置）
+        */
+        public const string PROXY_URL = "http://0.0.0.0:0";
+
+        //=======【上报信息配置】===================================
+        /* 测速上报等级，0.关闭上报; 1.仅错误时上报; 2.全量上报
+        */
+        public const int REPORT_LEVENL = 1;
+
+        //=======【日志级别】===================================
+        /* 日志等级，0.不输出日志；1.只输出错误信息; 2.输出错误和正常信息; 3.输出错误信息、正常信息和调试信息
+        */
+        public const int LOG_LEVENL = 3;
+    }
+}
